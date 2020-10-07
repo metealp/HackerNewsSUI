2cf5dce5cc5d6140555cd4dd1d50618a8ecae043
Mete Alp Kızılçay <kizilcaymetealp@gmail.com>
2020-10-07 22:52:34 +0300

    Initial Commit

---
+++ HackerNewsSUI/HackerNewsSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsSUIApp.swift
+//  HackerNewsSUI
+//
+//  Created by Mete Alp Kizilcay on 7.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
